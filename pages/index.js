4e0f391e491d041b91bd6c6559c352239d6e3f49
robupe <robertobupe@gmail.com>
2020-12-02 16:04:22 +0100

    other

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import styles from '../styles.module.css'
 export default function Home() {
   return (
     <div className={styles.hello}>
-      <p>Hello World</p>
+      <p>Hello!!</p>
     </div>
   )
 }
